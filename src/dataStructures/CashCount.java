8413914f648bc177b575fefe2ea9f8d0b5a62541
ngoguened <s2013017@ed.ac.uk>
2021-04-12 17:33:22 +0000

    CashCount constants

--- src/dataStructures/CashCount.java
+++ src/dataStructures/CashCount.java
@@ -4,82 +4,93 @@ package dataStructures;
  * The implementation of ICashCount.
  */
 public class CashCount implements ICashCount {
+
     /**
-     * The storage of number of each type of cash. Index 0 = 20 pound notes, index 1
-     * = 10 pound notes, index 2 = 5 pound notes, index 3 = 2 pound coins, index 4 =
-     * 1 pound coins, index 5 = 50 pence coins. index 6 = 20 pence coins, index 7 =
-     * 10 pence coins.
+     * Index constants for each cash value in cashSupply.
+     */
+    public static final int TWENTY_POUND_NOTE = 0;
+    public static final int TEN_POUND_NOTE = 1;
+    public static final int FIVE_POUND_NOTE = 2;
+    public static final int TWO_POUND_COIN = 3;
+    public static final int ONE_POUND_COIN = 4;
+    public static final int FIFTY_P_COIN = 5;
+    public static final int TWENTY_P_COIN = 6;
+    public static final int TEN_P_COIN = 7;
+
+    /**
+     * The storage of number of each type of cash.
      */
     private int[] cashSupply = new int[8];
+    
 
     /**
      * @param noteCount the notecount for each respective type of cash.
      */
     public void setNrNotes_20pounds(int noteCount) {
-        cashSupply[0] = noteCount;
+        cashSupply[TWENTY_POUND_NOTE] = noteCount;
     }
 
     public void setNrNotes_10pounds(int noteCount) {
-        cashSupply[1] = noteCount;
+        cashSupply[TEN_POUND_NOTE] = noteCount;
     }
 
     public void setNrNotes_5pounds(int noteCount) {
-        cashSupply[2] = noteCount;
+        cashSupply[FIVE_POUND_NOTE] = noteCount;
     }
 
     public void setNrCoins_2pounds(int coinCount) {
-        cashSupply[3] = coinCount;
+        cashSupply[TWO_POUND_COIN] = coinCount;
     }
 
     public void setNrCoins_1pound(int coinCount) {
-        cashSupply[4] = coinCount;
+        cashSupply[ONE_POUND_COIN] = coinCount;
     }
 
     public void setNrCoins_50p(int coinCount) {
-        cashSupply[5] = coinCount;
+        cashSupply[FIFTY_P_COIN] = coinCount;
     }
 
     public void setNrCoins_20p(int coinCount) {
-        cashSupply[6] = coinCount;
+        cashSupply[TWENTY_P_COIN] = coinCount;
     }
 
     public void setNrCoins_10p(int coinCount) {
-        cashSupply[7] = coinCount;
+        cashSupply[TEN_P_COIN] = coinCount;
     }
 
     /**
      * @return the notecount for each respective type of cash.
      */
     public int getNrNotes_20pounds() {
-        return cashSupply[0];
+        return cashSupply[TWENTY_POUND_NOTE];
     }
 
     public int getNrNotes_10pounds() {
-        return cashSupply[1];
+        return cashSupply[TEN_POUND_NOTE];
     }
 
     public int getNrNotes_5pounds() {
-        return cashSupply[2];
+        return cashSupply[FIVE_POUND_NOTE];
     }
 
     public int getNrCoins_2pounds() {
-        return cashSupply[3];
+        return cashSupply[TWO_POUND_COIN];
     }
 
     public int getNrCoins_1pound() {
-        return cashSupply[4];
+        return cashSupply[ONE_POUND_COIN];
     }
 
     public int getNrCoins_50p() {
-        return cashSupply[5];
+        return cashSupply[FIFTY_P_COIN];
     }
 
     public int getNrCoins_20p() {
-        return cashSupply[6];
+        return cashSupply[TWENTY_P_COIN];
     }
 
     public int getNrCoins_10p() {
-        return cashSupply[7];
+        return cashSupply[TEN_P_COIN];
     }
 
 }
